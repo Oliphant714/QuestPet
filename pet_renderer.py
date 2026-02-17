e56dccdc8d60361dfb485f9c59c63cd81a31d81b
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2026-02-17 16:14:42 -0700

    resized all visuals

--- pet_renderer.py
+++ pet_renderer.py
@@ -4,7 +4,7 @@ import sys
 pygame.init()
 
 #Window settings
-WIDTH, HEIGHT = 320, 320
+WIDTH, HEIGHT = 64, 64
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("QuestPet")
 
@@ -12,8 +12,8 @@ clock = pygame.time.Clock()
 
 idle_frames = []
 
-for i in range(1, 4):
-    idle_frame = pygame.image.load(f"Visuals/assets/idle/i_{i}_blob.png").convert_alpha()
+for i in range(0, 3):
+    idle_frame = pygame.image.load(f"Visuals/assets/idle/i_blob_{i}.png").convert_alpha()
     idle_frames.append(idle_frame)
 
 frame_index = 0
@@ -28,7 +28,7 @@ while running:
     screen.fill((0, 0, 0))
 
     #Draw the idle animation
-    screen.blit(idle_frames[frame_index](0, 0))
+    screen.blit(idle_frames[frame_index], (1, 1))
 
     #Advance animation
     frame_index += 1
